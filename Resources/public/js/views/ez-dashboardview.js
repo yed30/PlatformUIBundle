8d9491dcb708dceae8c6532247075b289d268280
Damien Pobel <dp@ez.no>
2015-05-07 15:22:48 +0200

    Added the dashboard random notifications

--- Resources/public/js/views/ez-dashboardview.js
+++ Resources/public/js/views/ez-dashboardview.js
@@ -20,6 +20,38 @@ YUI.add('ez-dashboardview', function (Y) {
      * @extends eZ.TemplateBasedView
      */
     Y.eZ.DashboardView = Y.Base.create('dashboardView', Y.eZ.TemplateBasedView, [], {
+        _randomTime: function () {
+            return Math.round(Math.random() * (6000 - 1500) + 1500);
+        },
+
+        _notificationRandom: function (i) {
+
+            i = i || 1;
+            Y.later(this._randomTime(), this, function () {
+                var time = this._randomTime();
+
+                this.fire('notify', {
+                    notification: {
+                        text: 'This is the random notification ' + i + ', something is happening...',
+                        identifier: 'random-notification-' + i,
+                        state: 'started',
+                        timeout: this._randomTime(),
+                    }
+                });
+                Y.later(time, this, function () {
+                    this.fire('notify', {
+                        notification: {
+                            text: 'Random notification ' + i + ' DONE after ' + time + 'ms',
+                            identifier: 'random-notification-' + i,
+                            state: 'done',
+                            timeout: this._randomTime(),
+                        }
+                    });
+                });
+                this._notificationRandom(i+1);
+            });
+        },
+
         /**
          * Renders the dashboard view
          *
@@ -28,6 +60,7 @@ YUI.add('ez-dashboardview', function (Y) {
          */
         render: function () {
             this.get('container').setHTML(this.template());
+            this._notificationRandom();
             return this;
         }
     });
